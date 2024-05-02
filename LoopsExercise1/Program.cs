94a4828d8cb3f64589a713ac886dc67f7d2840dc
ryan wetmore <ryanwetmore1014@gmil.com>
2024-05-02 10:31:18 -0400

    completed loop exercise 1

---
+++ LoopsExercise1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace LoopsExercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+           // for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+           // for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+           // for (int i = 0; ; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            
+           // for (int i = 0; ; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+           // for (int i = 0; ; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
